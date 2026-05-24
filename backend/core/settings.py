e474845bff7ab491dc63664b2d072cee570a5605
Your Name <you@example.com>
2026-05-24 03:22:27 +0000

    feat:app services

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework', ## IMPLEMENTA DJANGO REST FRAMEWORK
-    'corsheaders' ## PERMITE LA CONEXION CON OTROS PUERTOS 
+    'corsheaders', ## PERMITE LA CONEXION CON OTROS PUERTOS 
+    
+    'apps.services', 
 ]
 
 MIDDLEWARE = [
