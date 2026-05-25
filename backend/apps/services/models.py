baf82329d8af63f9b8f0d032924edb33e00a6a03
Your Name <you@example.com>
2026-05-25 04:49:49 +0000

    fead:servicio order

--- backend/apps/services/models.py
+++ backend/apps/services/models.py
@@ -1,3 +1,124 @@
 from django.db import models
+from decimal import Decimal
 
 # Create your models here.
+
+#------ GENERA LA INFORMACION DEL TECNICO --------
+class Tecnico(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+#--------------------------------------------------
+
+class OrdenServicio(models.Model):
+    class StatusChoice(models.TextChoices):
+        PENDING = 'pending','Pendiente'
+        PAID = 'paid','Pagado'
+    
+    class PaymentMethodChoices(models.TextChoices):
+        CASH = 'cash', 'Efectivo'
+        TRANSFER = 'transfer' , 'Transferencia'
+        CARD = 'card' , 'Tarjeta'
+    
+    numero_orden = models.CharField(
+        max_length=5,
+        unique=True
+    )
+    
+    tecnico = models.ForeignKey(
+        Tecnico,
+        on_delete=models.CASCADE,
+        related_name='Orden_servicio'
+    )
+    
+    status = models.CharField(
+        max_length=20,
+        choices=StatusChoice.choices,
+        default=StatusChoice.PENDING
+    )
+    
+    costo_servicio = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    
+    facturado = models.BooleanField(default=False)
+    
+    metodo_pago = models.CharField(
+        max_length=20,
+        choices=PaymentMethodChoices.choices,
+        default=PaymentMethodChoices.CASH)
+    
+    subtotal = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    
+    iva = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    
+    total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    
+    comision = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    
+    ganancia_taller = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    
+    fecha_cierre = models.DateTimeField(
+        blank=True,
+        null=True
+    )
+    
+    def save(self, *args, **kwargs):
+        
+        self.subtotal = self.costo_servicio
+        
+        if self.facturado:
+            self.iva = self.costo_servicio * Decimal('0.16')
+        else:
+            self.iva = Decimal('0.00')
+        
+        self.total = self.subtotal + self.iva
+        
+        self.comision = self.costo_servicio / Decimal('2')
+        self.ganancia_taller = self.costo_servicio / Decimal('2')
+        
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        
+        facturado_str = "Sí" if self.facturado else "No"
+        
+        fecha_c = self.fecha_creacion.strftime('%d/%m/%Y') if self.fecha_creacion else "N/A"
+        fecha_f = self.fecha_cierre.strftime('%d/%m/%Y') if self.fecha_cierre else "N/A"
+        
+        
+        
+        return (
+        f"Orden: {self.numero_orden} | "
+        f"Técnico: {self.tecnico} | "
+        f"Pago: {self.metodo_pago} | "
+        f"Total: ${self.total} | "
+        f"Estatus: {self.status} | "
+        f"Facturado: {facturado_str} | "
+        f"Creación: {fecha_c} | "
+        f"Cierre: {fecha_f}"
+    )
